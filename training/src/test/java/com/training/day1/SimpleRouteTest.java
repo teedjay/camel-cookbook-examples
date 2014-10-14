cd038390ff8067a7e57ceeb8160b1cf691290939
Thore Johnsen <thore.johnsen@unit4.com>
2014-10-14 14:37:49 +0200

    Extracted uri constants

--- training/src/test/java/com/training/day1/SimpleRouteTest.java
+++ training/src/test/java/com/training/day1/SimpleRouteTest.java
@@ -10,13 +10,16 @@ import static org.junit.Assert.*;
 
 public class SimpleRouteTest {
 
+    public static final String DIRECT_IN = "direct:in";
+    public static final String MOCK_OUT = "mock:out";
+
     private CamelContext context;
 
     @org.junit.Before
     public void setUp() throws Exception {
         SimpleRoute simpleRoute = new SimpleRoute();
-        simpleRoute.setStartUri("direct:in");
-        simpleRoute.setEndUri("mock:out");
+        simpleRoute.setStartUri(DIRECT_IN);
+        simpleRoute.setEndUri(MOCK_OUT);
         context = new DefaultCamelContext();
         context.addRoutes(simpleRoute);
         context.start();
@@ -31,13 +34,13 @@ public class SimpleRouteTest {
     public void testSimpleRoute() throws Exception {
 
         // set some expectations
-        MockEndpoint mockOut = context.getEndpoint("mock:out", MockEndpoint.class);
+        MockEndpoint mockOut = context.getEndpoint(MOCK_OUT, MockEndpoint.class);
         mockOut.setExpectedMessageCount(1);             // one message
         mockOut.message(0).body().isEqualTo("I got: Hello");   // expect some content
 
         // send a message
         ProducerTemplate template = context.createProducerTemplate();
-        template.sendBody("direct:in", "Hello");
+        template.sendBody(DIRECT_IN, "Hello");
 
         // assert out expectations
         mockOut.assertIsSatisfied();
