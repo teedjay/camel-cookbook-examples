ae385f31223e767fce175eaaea7618787e5c758b
Thore Johnsen <thore.johnsen@unit4.com>
2014-10-15 15:49:21 +0200

    Adding enricher

--- training/src/main/java/com/training/day2/EnricherRoute.java
+++ training/src/main/java/com/training/day2/EnricherRoute.java
@@ -1,6 +1,8 @@
 package com.training.day2;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
 
 /**
  * Simple bean binding example - can be tested as a simple POJO without Camel
@@ -9,35 +11,36 @@ import org.apache.camel.builder.RouteBuilder;
 public class EnricherRoute extends RouteBuilder {
 
     private String startUri;
+    private String serviceUri;
     private String endUri;
 
     public void setStartUri(String startUri) {
         this.startUri = startUri;
     }
 
+    public void setServiceUri(String serviceUri) {
+        this.serviceUri = serviceUri;
+    }
+
     public void setEndUri(String endUri) {
         this.endUri = endUri;
     }
 
+
     @Override
     public void configure() throws Exception {
 
-        from(startUri).routeId("greeterRoute")
-            .choice()
-                .when(simple("${header[locale]} == 'no'"))
-                    .transform(simple("Hallo: ${body}"))
-                .when(simple("${header[locale]} == 'se'"))
-                    .transform(simple("Bork bork bork: ${body}"))
-                .when(simple("${header[locale]} == 'ru'"))
-                    .to("direct:russian")
-                .otherwise()
-                    .transform(simple("Hello: ${body}"))
-            .end()
-            .setHeader("additionalGreeting", body())
+        from(startUri).routeId("enricherRoute")
+            .enrich(serviceUri, new AggregationStrategy() {
+                @Override
+                public Exchange aggregate(Exchange exchange0, Exchange exchange1) {
+                    String body1 = exchange0.getIn().getBody(String.class); // the main route
+                    String body2 = exchange1.getIn().getBody(String.class); // the enricher route
+                    exchange0.getIn().setBody(body1 + ":" + body2);
+                    return exchange0;
+                }
+            })
             .to(endUri);
-
-        from("direct:russian").routeId("russianLogging")
-            .log("I have no idea on how to speak russian").id("logRussian");
 
     }
 
--- training/src/test/java/com/training/day2/EnricherRouteTest.java
+++ training/src/test/java/com/training/day2/EnricherRouteTest.java
@@ -1,8 +1,6 @@
 package com.training.day2;
 
-import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
+import org.apache.camel.*;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
@@ -15,80 +13,43 @@ import org.junit.Test;
 public class EnricherRouteTest extends CamelTestSupport {
 
     public static final String DIRECT_IN = "direct:in";
+    public static final String MOCK_SERVICE = "mock:service";
     public static final String MOCK_OUT = "mock:out";
-
-    @EndpointInject(uri = MOCK_OUT)
-    MockEndpoint mockOut;
 
     @Produce(uri = DIRECT_IN)
     ProducerTemplate in;
 
-    @Override
-    public boolean isUseAdviceWith() {
-        return true;
-    }
+    @EndpointInject(uri = MOCK_SERVICE)
+    MockEndpoint mockService;
 
-
+    @EndpointInject(uri = MOCK_OUT)
+    MockEndpoint mockOut;
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
-        GreeterRoute greeterRoute = new GreeterRoute();
-        greeterRoute.setStartUri(DIRECT_IN);
-        greeterRoute.setEndUri(MOCK_OUT);
-        return greeterRoute;
+        EnricherRoute route = new EnricherRoute();
+        route.setStartUri(DIRECT_IN);
+        route.setServiceUri(MOCK_SERVICE);
+        route.setEndUri(MOCK_OUT);
+        return route;
     }
 
     @Test
-    public void testProcessor_noLocale() throws Exception {
-        context.start();
+    public void testEnricher() throws Exception {
+
         mockOut.setExpectedMessageCount(1);
-        mockOut.message(0).body().isEqualTo("Hello: Some Body");
-        mockOut.message(0).header("additionalGreeting").isEqualTo("Hello: Some Body");
-        in.sendBody("Some Body");
-        assertMockEndpointsSatisfied();
-    }
+        mockOut.message(0).body().isEqualTo("Ping:Pong");
 
-    @Test
-    public void testProcessor_ru() throws Exception {
-
-        context.getRouteDefinition("russianLogging")
-                .adviceWith(context, new AdviceWithRouteBuilder() {
+        mockService.setExpectedMessageCount(1);
+        mockService.whenExchangeReceived(1, new Processor() {
                     @Override
-                    public void configure() throws Exception {
-                        weaveById("logRussian").before().to("mock:logRussian");
+                    public void process(Exchange exchange) throws Exception {
+                        exchange.getIn().setBody("Pong");
                     }
-                });
+                }
+            );
 
-        context.start();
-
-        mockOut.setExpectedMessageCount(1);
-        mockOut.message(0).body().isEqualTo("Some Body");
-        mockOut.message(0).header("additionalGreeting").isEqualTo("Some Body");
-
-        MockEndpoint mockRussian = getMockEndpoint("mock:logRussian");
-        mockRussian.setExpectedMessageCount(1);
-
-        in.sendBodyAndHeader("Some Body", "locale", "ru");
-
-        assertMockEndpointsSatisfied();
-
-    }
-
-    @Test
-    public void testProcessor_no() throws Exception {
-        context.start();
-        mockOut.setExpectedMessageCount(1);
-        mockOut.message(0).body().isEqualTo("Hallo: Some Body");
-        in.sendBodyAndHeader("Some Body", "locale", "no");
-        assertMockEndpointsSatisfied();
-    }
-
-    @Test
-    public void testProcessor_se() throws Exception {
-        context.start();
-        mockOut.setExpectedMessageCount(1);
-        mockOut.message(0).body().isEqualTo("Bork bork bork: Some Body");
-        in.sendBodyAndHeader("Some Body", "locale", "se");
+        in.sendBody("Ping");
         assertMockEndpointsSatisfied();
     }
 
