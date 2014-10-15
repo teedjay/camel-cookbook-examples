4d1308f8410017b1b6a2044fba9f8a73a2b58aa4
Thore Johnsen <thore.johnsen@unit4.com>
2014-10-15 14:02:41 +0200

    fixed some git references

---
+++ training/src/main/java/com/training/day2/WiretapRoute.java
@@ -0,0 +1,55 @@
+package com.training.day2;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Adding wire tapping - for auditing in the background and other uses.
+ * @author thore
+ */
+public class WireTapRoute extends RouteBuilder {
+
+    private String startUri;
+    private String endUri;
+    private String auditUri;
+
+    public void setStartUri(String startUri) {
+        this.startUri = startUri;
+    }
+
+    public void setEndUri(String endUri) {
+        this.endUri = endUri;
+    }
+
+    public void setAuditUri(String auditUri) {
+        this.auditUri = auditUri;
+    }
+
+    @Override
+    public void configure() throws Exception {
+
+        from(startUri).routeId("greeterRoute").startupOrder(30)
+            .choice()
+                .when(simple("${header[locale]} == 'no'"))
+                    .wireTap("direct:audit")
+                    .transform(simple("Hallo: ${body}"))
+                .endChoice()
+                .when(simple("${header[locale]} == 'se'"))
+                    .transform(simple("Bork bork bork: ${body}"))
+                .when(simple("${header[locale]} == 'ru'"))
+                    .to("direct:russian")
+                .otherwise()
+                    .transform(simple("Hello: ${body}"))
+            .end()
+            .setHeader("additionalGreeting", body())
+            .to(endUri);
+
+        from("direct:russian").routeId("russianLogging").startupOrder(20)
+            .log("I have no idea on how to speak russian").id("logRussian");
+
+        from("direct:audit").routeId("audit").startupOrder(10)
+            .to(auditUri)
+            .log("Audit system responded : ${body}");
+
+    }
+
+}
