18125582719b28fca204a1559cfe39b595b49a6a
Thore Johnsen <thore.johnsen@unit4.com>
2014-10-16 15:30:34 +0200

    Idempotency using the in memory repository

---
+++ training/src/main/java/com/training/day3/IdempotentRoute.java
@@ -0,0 +1,32 @@
+package com.training.day3;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.idempotent.MemoryIdempotentRepository;
+
+/**
+ * Route template for testing new things.
+ * @author thore
+ */
+public class IdempotentRoute extends RouteBuilder {
+
+    private String startUri;
+    private String endUri;
+
+    public void setStartUri(String startUri) {
+        this.startUri = startUri;
+    }
+
+    public void setEndUri(String endUri) {
+        this.endUri = endUri;
+    }
+
+    @Override
+    public void configure() throws Exception {
+
+        from(startUri).routeId("idempotentRoute")
+            .idempotentConsumer(body(), new MemoryIdempotentRepository())
+            .to(endUri);
+
+    }
+
+}
---
+++ training/src/test/java/com/training/day3/IdempotentRouteTest.java
@@ -0,0 +1,56 @@
+package com.training.day3;
+
+import org.apache.camel.*;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+/**
+ * Camel has built-in test support that does setup / tear down, creates mock endpoints etc
+ */
+public class IdempotentRouteTest extends CamelTestSupport {
+
+    public static final String DIRECT_IN = "direct:in";
+    public static final String MOCK_OUT = "mock:out";
+
+    @Produce(uri = DIRECT_IN)
+    ProducerTemplate in;
+
+    @EndpointInject(uri = MOCK_OUT)
+    MockEndpoint mockOut;
+
+    @Override
+    public boolean isUseDebugger() {
+        return true;
+    }
+
+    @Override
+    protected void debugBefore(Exchange exchange, Processor processor, ProcessorDefinition<?> definition, String id, String label) {
+        super.debugBefore(exchange, processor, definition, id, label);
+    }
+
+    @Override
+    protected RouteBuilder[] createRouteBuilders() throws Exception {
+        IdempotentRoute route = new IdempotentRoute();
+        route.setStartUri(DIRECT_IN);
+        route.setEndUri(MOCK_OUT);
+        return new RouteBuilder[] { route };
+    }
+
+    @Test
+    public void testRoute() throws Exception {
+
+        mockOut.setExpectedMessageCount(3);
+        mockOut.expectedBodiesReceived("cheese", "chocolate", "wine");
+
+        in.sendBody("cheese");
+        in.sendBody("chocolate");
+        in.sendBody("wine");
+        in.sendBody("cheese");
+
+        assertMockEndpointsSatisfied(); // asserts all injected mocks
+    }
+
+}
