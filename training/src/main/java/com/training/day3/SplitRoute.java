3aa7ada917d129afaf07b44d0e514d2814d93d42
Thore Johnsen <thore.johnsen@unit4.com>
2014-10-16 13:19:24 +0200

    Adding a template for starting new tests and a starting point for testing splitter.

---
+++ training/src/main/java/com/training/day3/SplitRoute.java
@@ -0,0 +1,30 @@
+package com.training.day3;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Route for testing splitting.
+ * @author thore
+ */
+public class SplitRoute extends RouteBuilder {
+
+    private String startUri;
+    private String endUri;
+
+    public void setStartUri(String startUri) {
+        this.startUri = startUri;
+    }
+
+    public void setEndUri(String endUri) {
+        this.endUri = endUri;
+    }
+
+    @Override
+    public void configure() throws Exception {
+
+        from(startUri).routeId("splitterRoute")
+            .to(endUri);
+
+    }
+
+}
